b9f66b4115f4c2d9d2272d4e8eb412fe3c6bbf86
Dalton Dayton <50755420+DaltonDayton@users.noreply.github.com>
2024-10-17 20:49:48 -0600

    fix split_nodes_delimiter

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -6,23 +6,20 @@ from src.textnode import TextNode, TextType
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for node in old_nodes:
-        if node.text_type is not TextType.TEXT:
+        if node.text_type != TextType.TEXT:
             new_nodes.append(node)
         else:
-            split_node = node.text.split(delimiter)
-            if len(split_node) is not 3:
+            sections = node.text.split(delimiter)
+            if len(sections) % 2 == 0:
                 raise Exception("Missing delimiter")
-            new_nodes.append(TextNode(split_node[0], TextType.TEXT))
-            match text_type:
-                case TextType.BOLD:
-                    new_nodes.append(TextNode(split_node[1], TextType.BOLD))
-                case TextType.CODE:
-                    new_nodes.append(TextNode(split_node[1], TextType.CODE))
-                case TextType.ITALIC:
-                    new_nodes.append(TextNode(split_node[1], TextType.ITALIC))
-                case TextType.ITALIC:
-                    new_nodes.append(TextNode(split_node[1], TextType.ITALIC))
-            new_nodes.append(TextNode(split_node[2], TextType.TEXT))
+
+            for i in range(len(sections)):
+                if sections[i] == "":
+                    continue
+                if i % 2 == 0:
+                    new_nodes.append(TextNode(sections[i], TextType.TEXT))
+                else:
+                    new_nodes.append(TextNode(sections[i], text_type))
     return new_nodes
 
 
